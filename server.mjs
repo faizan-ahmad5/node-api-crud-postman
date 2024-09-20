b17c17988f486542df9f8f840d329b2ab450205f
Faizan Ahmad Khan <fa3n20004@gmail.com>
2024-09-20 22:32:19 +0500

    Implement GET route for /api/items

--- server.mjs
+++ server.mjs
@@ -6,6 +6,13 @@ const server = createServer((req, res) => {
   console.log({ method: req.method, url: req.url });
   const { method, url } = req;
   res.setHeader("Content-Type", "application/json");
+  const parsedUrl = new URL(url, `http://${req.headers.host}`);
+
+  // get request
+  if (method === "GET" && parsedUrl.pathname === "/api/items") {
+    res.statusCode = 200;
+    res.end(JSON.stringify({ message: "GET request - Fetching all items" }));
+  }
 
   res.statusCode = 200;
   res.end("Server is running");
